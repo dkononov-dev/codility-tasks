6085c428a965ca790dd8f6d057e1f3550787a6fe
dkononov-dev <dmitri_kononov@outlook.com>
2019-08-03 13:25:06 +1000

    Use assertThat in unit test.

--- src/test/java/codility/tasks/BinaryGapTest.java
+++ src/test/java/codility/tasks/BinaryGapTest.java
@@ -3,7 +3,8 @@ package codility.tasks;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class BinaryGapTest {
 
@@ -16,16 +17,14 @@ public class BinaryGapTest {
 
     @Test
     public void test() {
-        assertEquals(0, binaryGap.solution(1));
-        assertEquals(0, binaryGap.solution(4));
-        assertEquals(1, binaryGap.solution(5));
-        assertEquals(2, binaryGap.solution(9));
-        assertEquals(0, binaryGap.solution(15));
-        assertEquals(1, binaryGap.solution(20));
-        assertEquals(0, binaryGap.solution(32));
-        assertEquals(4, binaryGap.solution(529));
-        assertEquals(5, binaryGap.solution(1041));
-
-
+        assertThat(binaryGap.solution(1), equalTo(0));
+        assertThat(binaryGap.solution(4), equalTo(0));
+        assertThat(binaryGap.solution(5), equalTo(1));
+        assertThat(binaryGap.solution(9), equalTo(2));
+        assertThat(binaryGap.solution(15), equalTo(0));
+        assertThat(binaryGap.solution(20), equalTo(1));
+        assertThat(binaryGap.solution(32), equalTo(0));
+        assertThat(binaryGap.solution(529), equalTo(4));
+        assertThat(binaryGap.solution(1041), equalTo(5));
     }
 }
